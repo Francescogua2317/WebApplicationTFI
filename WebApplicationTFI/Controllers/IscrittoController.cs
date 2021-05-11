bcfeaa2727e1b6372a5fc5f403735268a8ef7b5e
Lanfranco Romei <lanfranco.romei@outlook.it>
2021-05-11 16:18:27 +0200

    Aggiornamento Richiesta TFR

--- WebApplicationTFI/Controllers/IscrittoController.cs
+++ WebApplicationTFI/Controllers/IscrittoController.cs
@@ -150,7 +150,71 @@ namespace WebApplicationTFI.Controllers
         public ActionResult RichiestaTFR()
         {
             if (Session["NomeUtente"] != null) ViewBag.NomeUtente = Session["NomeUtente"].ToString();
-            return View();
+            string codfis = Session["CodiceFiscale"].ToString();
+            DataLayer objDataAccess = new DataLayer();
+            string strSQL = "SELECT C.SIGPRO, C.DENCOM, C.CODCOM FROM CODLOC_APP C, ISCTWEB I WHERE I.CODCOMNAS=C.CODCOM AND I.CODFIS='" + codfis + "'";
+            string strSQL2 = "SELECT MAT, COGNOME, NOME, CODFIS, DATNAS, SES, STAESTNAS, IND, NUMCIV, CAP, EMAIL, EMAILCERT, CELL, DENLOC, SIGPRO FROM ISCTWEB WHERE CODFIS='" + codfis + "'";
+            string strSQL3 = "SELECT T.DENTITSTU FROM TITSTU T, ISCTWEB I WHERE I.CODTITSTU=T.CODTITSTU";
+
+
+            DataSet objAnagrafica = new DataSet();
+            DataSet objAnagrafica2 = new DataSet();
+            DataSet objAnagrafica3 = new DataSet();
+
+
+            string errore = "";
+            objAnagrafica = objDataAccess.GetDataSet(strSQL, ref errore);
+            objAnagrafica2 = objDataAccess.GetDataSet(strSQL2, ref errore);
+            objAnagrafica3 = objDataAccess.GetDataSet(strSQL3, ref errore);
+
+
+            string mat = objAnagrafica2.Tables[0].Rows[0]["MAT"].ToString();
+            string nome = objAnagrafica2.Tables[0].Rows[0]["NOME"].ToString();
+            string cognome = objAnagrafica2.Tables[0].Rows[0]["COGNOME"].ToString();
+            string codiceFiscale = objAnagrafica2.Tables[0].Rows[0]["CODFIS"].ToString();
+            string sesso = objAnagrafica2.Tables[0].Rows[0]["SES"].ToString();
+            string dataNascita = objAnagrafica2.Tables[0].Rows[0]["DATNAS"].ToString().Substring(0,10);
+            string statoEsteroNascita = objAnagrafica2.Tables[0].Rows[0]["STAESTNAS"].ToString();
+            string titoloStudio = objAnagrafica3.Tables[0].Rows[0]["DENTITSTU"].ToString();
+            string indirizzo = objAnagrafica2.Tables[0].Rows[0]["IND"].ToString();
+            string numeroCivico = objAnagrafica2.Tables[0].Rows[0]["NUMCIV"].ToString();
+            string cap = objAnagrafica2.Tables[0].Rows[0]["CAP"].ToString();
+            string comune = objAnagrafica2.Tables[0].Rows[0]["DENLOC"].ToString();
+            string sigpro2 = objAnagrafica2.Tables[0].Rows[0]["SIGPRO"].ToString();
+            string email = objAnagrafica2.Tables[0].Rows[0]["EMAIL"].ToString();
+            string emailCert = objAnagrafica2.Tables[0].Rows[0]["EMAILCERT"].ToString();
+            string cellulare = objAnagrafica2.Tables[0].Rows[0]["CELL"].ToString();
+
+
+            if (Utente.queryOk(objAnagrafica2))
+            {
+                
+                ViewData["Matricola"] = mat;
+                ViewData["Cognome"] = cognome;
+                ViewData["Nome"] = nome;
+                ViewData["CodiceFis"] = codiceFiscale;
+                ViewData["DataNascita"] = dataNascita;
+                ViewData["Sesso"] = sesso;
+                ViewData["StatoEstero"] = statoEsteroNascita;
+                ViewData["TitoloStudio"] = titoloStudio;
+                ViewData["Indirizzo"] = "Via " +  indirizzo + " "+ numeroCivico;
+                ViewData["Cap"] = cap;
+                ViewData["Comune2"] = comune;
+                ViewData["Provincia2"] = sigpro2;
+                ViewData["Email"] = email;
+                ViewData["EmailCert"] = emailCert;
+                ViewData["Cellulare"] = cellulare;
+                
+
+            }
+            if (Session["layout"].ToString() == "~/Views/Shared/_AmministrativoLayout.cshtml")
+            {
+                return View("~/Views/Iscritto/Anagrafica.cshtml", "~/Views/Shared/_AmministrativoLayout.cshtml");
+            }
+            else
+            {
+                return View();
+            }
         }
         public ActionResult AnticipazioneTFR()
         {
