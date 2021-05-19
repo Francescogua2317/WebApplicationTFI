8398229e3c61393af7bbb8e67be056480f8b6845
tsi-enpaia <tsi-empaia@outlook.it>
2021-05-19 16:40:11 +0200

    varie

---
+++ WebApplicationTFI/Utilities/ModuliHelper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using iText;
+
+namespace WebApplicationTFI.Utilities
+{
+    public static class ModuliHelper
+    {
+        
+    }
+}
